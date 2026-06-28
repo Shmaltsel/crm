f78f4a67667663e7de21785821714c43cacdfcad
Arthur <shmaltsell@gmail.com>
2026-06-29 01:56:28 +0300

    оптимізація

--- apps/backend/prisma/prisma.mock.ts
+++ apps/backend/prisma/prisma.mock.ts
@@ -1,5 +1,5 @@
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 // Типізація для нашого моку, щоб автодоповнення працювало в тестах
 export type MockPrismaService = DeepMockProxy<PrismaService>;
