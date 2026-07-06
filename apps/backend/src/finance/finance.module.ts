4391a85eb723fb907ebd63e96e2b794c8874b887
Arthur <shmaltsell@gmail.com>
2026-07-06 13:38:25 +0300

    підготовка до прода

--- apps/backend/src/finance/finance.module.ts
+++ apps/backend/src/finance/finance.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { RedisCacheModule } from '../common/cache/redis-cache.module';
 import { FinanceController } from './finance.controller';
 import { FinanceService } from './finance.service';
 
 @Module({
+  imports: [RedisCacheModule],
   controllers: [FinanceController],
   providers: [FinanceService],
 })
